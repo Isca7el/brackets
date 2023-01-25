331377607fcb1a4e81ee9fd9e3356b4f57422fd9
Demichshuk Gleb <coldfear457@gmail.com>
2023-01-25 16:31:43 +0600

    feat add solution brackets

--- src/index.js
+++ src/index.js
@@ -1,29 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-    let openBracket = [],
-    closeBraket = [];
-for (let i = 0; i < bracketsConfig.length; i++) {
+
+  let openBracket = [],
+    closeBracket = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
     openBracket[i] = bracketsConfig[i][0];
-    closeBraket[i] = bracketsConfig[i][1];
-}
-    let stack = [];
-  for (let m = 0; m < str.length; m ++) {
+    closeBracket[i] = bracketsConfig[i][1];
+  }
+
+  let stack = [];
+
+  for (let m = 0; m < str.length; m++) {
     let currentSymbol = str[m];
 
-    if (openBracket.includes(currentSymbol)){
-      stack.push(currentSymbol);
-    } else {
-      if (stack.length === 0){
-        return false;
+    if (openBracket.includes(currentSymbol) ){
+      if(stack.length > 0){
+        let topElement = stack[stack.length - 1];
+        let indexTopElement = openBracket.indexOf(topElement);
+        let indexCurrentSymbol = closeBracket.indexOf(currentSymbol);
+        if(indexCurrentSymbol === indexTopElement){
+          stack.pop(topElement);
+        } else {
+          stack.push(currentSymbol);
+        }
+      }else {
+        stack.push(currentSymbol);
       }
 
-      let topElement = stack[stack.length -1];
-
-      if (closeBraket[currentSymbol] === topElement) {
-        stack.pop();
-      } else {
+    } else {
+      let topElement = stack[stack.length - 1];
+      let indexTopElement = openBracket.indexOf(topElement);
+      let indexCurrentSymbol = closeBracket.indexOf(currentSymbol);
+      if (stack.length === 0) {
         return false;
+      }
+      if(indexCurrentSymbol === indexTopElement){
+        stack.pop(topElement);
+      } else {
+        stack.push(currentSymbol);
       }
     }
   }
+
   return stack.length === 0;
 }
